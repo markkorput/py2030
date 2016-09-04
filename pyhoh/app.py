3c9ecea024bd167c4b548a25571f3b3b3cd1ceef
Mark van de Korput <dr.theman@gmail.com>
2016-09-04 18:42:45 +0200

    run using ./pyhoh/app.py

--- pyhoh/app.py
+++ pyhoh/app.py
@@ -1,90 +1,14 @@
-import copy
-from datetime import datetime
-import logging
-logging.basicConfig(level=logging.WARNING)
-logger = logging.getLogger(__name__)
-
-from utils.config_file import ConfigFile
-
-class App:
-    def __init__(self, options = {}):
-        # config
-        self.options = options
-        self.profile = self.options['profile'] if 'profile' in self.options else 'default'
-
-        # attributes
-        self.config_file = ConfigFile('config/config.yml')
-        self.components = []
-
-    def __del__(self):
-        self.destroy()
-
-    def setup(self):
-        # read config file content
-        self.config_file.load()
-
-        # apply config data
-        self._apply_config(self.config_file)
-
-    def destroy(self):
-        for comp in self.components:
-            comp.destroy()
-
-    def update(self):
-        for comp in self.components:
-            comp.update()
-
-    def _apply_config(self, config_file):
-        # read profile data form config file
-        profile_data = config_file.get_value('pyhoh.profiles.'+self.profile)
-        if not profile_data:
-            profile_data = {}
-
-        omxvideo = None
-        if 'omxvideo' in profile_data:
-            from components.omxvideo import OmxVideo
-            omxvideo = OmxVideo(profile_data['omxvideo'])
-            self.components.append(omxvideo)
-            del OmxVideo
-
-        if 'omxvideo_osc_inputs' in profile_data:
-            from components.omx_video_osc_input import OmxVideoOscInput
-
-            # loop over each osc_input profile
-            for data in profile_data['omxvideo_osc_inputs'].values():
-                comp = OmxVideoOscInput(data)
-                comp.set_omxvideo(omxvideo)
-                comp.setup()
-                self.components.append(comp) # auto-starts
-
-            del OmxVideoOscInput
-
-        if 'omxsync' in profile_data:
-            from components.omxsync import OmxSync
-            comp = OmxSync(profile_data['omxsync'])
-            comp.setup(omxvideo)
-            self.components.append(comp)
-            del OmxSync
-
-        if 'osc_inputs' in profile_data:
-            from components.osc_input import OscInput
-
-            # loop over each osc_input profile
-            for data in profile_data['osc_inputs'].values():
-                comp = OscInput(data)
-                comp.setup()
-                self.components.append(comp) # auto-starts
-
-            del OscInput
+#!/usr/bin/env python
+from component_manager import ComponentManager
 
 if __name__ == '__main__':
-    app = App()
-    app.setup()
+    cm = ComponentManager()
+    cm.setup()
 
     try:
-        while True:
-            app.update()
+        while cm.running:
+            cm.update()
     except KeyboardInterrupt:
         print 'KeyboardInterrupt. Quitting.'
 
-    app.destroy()
+    cm.destroy()
---
+++ pyhoh/component_manager.py
@@ -0,0 +1,80 @@
+import copy
+from datetime import datetime
+import logging
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+from utils.config_file import ConfigFile
+
+class ComponentManager:
+    def __init__(self, options = {}):
+        # config
+        self.options = options
+        self.profile = self.options['profile'] if 'profile' in self.options else 'default'
+
+        # attributes
+        self.config_file = ConfigFile('config/config.yml')
+        self.components = []
+
+        self.running = True
+
+    def __del__(self):
+        self.destroy()
+
+    def setup(self):
+        # read config file content
+        self.config_file.load()
+
+        # apply config data
+        self._apply_config(self.config_file)
+
+    def destroy(self):
+        for comp in self.components:
+            comp.destroy()
+
+    def update(self):
+        for comp in self.components:
+            comp.update()
+
+    def _apply_config(self, config_file):
+        # read profile data form config file
+        profile_data = config_file.get_value('pyhoh.profiles.'+self.profile)
+        if not profile_data:
+            profile_data = {}
+
+        omxvideo = None
+        if 'omxvideo' in profile_data:
+            from components.omxvideo import OmxVideo
+            omxvideo = OmxVideo(profile_data['omxvideo'])
+            self.components.append(omxvideo)
+            del OmxVideo
+
+        if 'omxvideo_osc_inputs' in profile_data:
+            from components.omx_video_osc_input import OmxVideoOscInput
+
+            # loop over each osc_input profile
+            for data in profile_data['omxvideo_osc_inputs'].values():
+                comp = OmxVideoOscInput(data)
+                comp.set_omxvideo(omxvideo)
+                comp.setup()
+                self.components.append(comp) # auto-starts
+
+            del OmxVideoOscInput
+
+        if 'omxsync' in profile_data:
+            from components.omxsync import OmxSync
+            comp = OmxSync(profile_data['omxsync'])
+            comp.setup(omxvideo)
+            self.components.append(comp)
+            del OmxSync
+
+        if 'osc_inputs' in profile_data:
+            from components.osc_input import OscInput
+
+            # loop over each osc_input profile
+            for data in profile_data['osc_inputs'].values():
+                comp = OscInput(data)
+                comp.setup()
+                self.components.append(comp) # auto-starts
+
+            del OscInput

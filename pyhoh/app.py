bdba02dc384b33798908be0eae4b0225b0910647
Mark van de Korput <dr.theman@gmail.com>
2016-09-04 20:21:07 +0200

    profile selectable through command line

--- pyhoh/app.py
+++ pyhoh/app.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
+from optparse import OptionParser
 from component_manager import ComponentManager
 
 if __name__ == '__main__':
-    cm = ComponentManager()
+    parser = OptionParser()
+    parser.add_option('-p', '--profile', dest='profile', default="default")
+    # parser.add_option('-f', '--file', dest='file', default=None)
+    parser.add_option('-v', '--verbose', dest='verbose', action="store_true", default=False)
+
+    options, args = parser.parse_args()
+
+    cm = ComponentManager(options)
     cm.setup()
 
     try:

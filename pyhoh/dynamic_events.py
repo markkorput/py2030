c2ce77ded9901ad4c6281f24ce1376f128f5e713
Mark van de Korput <dr.theman@gmail.com>
2016-11-30 17:47:55 +0100

    DynamicEvents implementation and unittests

--- pyhoh/dynamic_events.py
+++ pyhoh/dynamic_events.py
@@ -2,4 +2,12 @@ from evento import Event
 
 class DynamicEvents:
     def __init__(self):
+        self._events = {}
         self.eventAddedEvent = Event()
+
+    def getEvent(self, id):
+        if id in self._events:
+            return self._events[id]
+        new_event = Event
+        self._events[id] = new_event
+        return new_event
--- tests/test_dynamic_events.py
+++ tests/test_dynamic_events.py
@@ -7,3 +7,19 @@ class TestDynamicEvents(unittest.TestCase):
     def test_init(self):
         dynevents = DynamicEvents()
         self.assertIsNotNone(dynevents.eventAddedEvent)
+        self.assertEqual(len(dynevents._events), 0)
+
+    def test_create_event(self):
+        dynevents = DynamicEvents()
+        self.assertEqual('foo' in dynevents._events, False)
+        event = dynevents.getEvent('foo')
+        self.assertEqual('foo' in dynevents._events, True)
+        self.assertEqual(dynevents._events['foo'], event)
+        self.assertEqual(len(dynevents._events), 1)
+
+    def test_get_existing_event(self):
+        dynevents = DynamicEvents()
+        event1 = dynevents.getEvent('bar')
+        event2 = dynevents.getEvent('bar')
+        self.assertEqual(event1, event2)
+        self.assertEqual(len(dynevents._events), 1)

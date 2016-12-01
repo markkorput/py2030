7ba3624b408a5c46f40d89605a31312e7c37c439
Mark van de Korput <dr.theman@gmail.com>
2016-12-01 13:37:29 +0100

    Some tests for DelayEvents, verifying expire, recycle and looing behaviour

--- tests/test_delay_events.py
+++ tests/test_delay_events.py
@@ -10,7 +10,7 @@ class TestEventToEvent(unittest.TestCase):
         self.assertIsNone(delay_events.dynamic_events)
 
     def test_setup(self):
-        delay_events = DelayEvents({'looper': {'source': 'loop', 'delay': 60, 'target': 'loop'}})
+        delay_events = DelayEvents({'example': {'source': 'loop', 'delay': 60, 'target': 'loop'}})
         dynamic_events = DynamicEvents()
         self.assertEqual(dynamic_events.getEvent('loop').getSubscriberCount(), 0)
         delay_events.setup(dynamic_events)
@@ -18,7 +18,7 @@ class TestEventToEvent(unittest.TestCase):
         self.assertEqual(dynamic_events.getEvent('loop').getSubscriberCount(), 1)
 
     def test_destroy(self):
-        delay_events = DelayEvents({'looper': {'source': 'loop', 'delay': 60, 'target': 'loop'}})
+        delay_events = DelayEvents({'example': {'source': 'loop', 'delay': 60, 'target': 'loop'}})
         dynamic_events = DynamicEvents()
         delay_events.setup(dynamic_events)
         self.assertEqual(delay_events.dynamic_events, dynamic_events)
@@ -28,7 +28,7 @@ class TestEventToEvent(unittest.TestCase):
         self.assertEqual(dynamic_events.getEvent('loop').getSubscriberCount(), 0)
 
     def test_update_without_trigger(self):
-        delay_events = DelayEvents({'looper': {'source': 'AA', 'delay': 60, 'target': 'BB'}})
+        delay_events = DelayEvents({'example': {'source': 'AA', 'delay': 60, 'target': 'BB'}})
         dynamic_events = DynamicEvents()
         delay_events.setup(dynamic_events)
         delay_events.update(62) # 'pretend' 62 seconds have passed
@@ -36,7 +36,7 @@ class TestEventToEvent(unittest.TestCase):
         self.assertEqual(dynamic_events.getEvent('BB')._fireCount, 0)
 
     def test_update_with_trigger(self):
-        delay_events = DelayEvents({'looper': {'source': 'AA', 'delay': 50, 'target': 'BB'}})
+        delay_events = DelayEvents({'example': {'source': 'AA', 'delay': 50, 'target': 'BB'}})
         dynamic_events = DynamicEvents()
         delay_events.setup(dynamic_events)
         # trigger source event, in order to trigger the delayed call to the target event
@@ -46,7 +46,7 @@ class TestEventToEvent(unittest.TestCase):
         self.assertEqual(dynamic_events.getEvent('BB')._fireCount, 1)
 
     def test_update_progress(self):
-        delay_events = DelayEvents({'looper': {'source': 'AA', 'delay': 30, 'target': 'BB'}})
+        delay_events = DelayEvents({'example': {'source': 'AA', 'delay': 30, 'target': 'BB'}})
         dynamic_events = DynamicEvents()
         delay_events.setup(dynamic_events)
         self.assertEqual(delay_events._delay_items[0].timer, 0)
@@ -67,7 +67,7 @@ class TestEventToEvent(unittest.TestCase):
         self.assertEqual(dynamic_events.getEvent('BB')._fireCount, 1)
 
     def test_update_progress_without_specified_dt(self):
-        delay_events = DelayEvents({'looper': {'source': 'AA', 'delay': 30, 'target': 'BB'}})
+        delay_events = DelayEvents({'example': {'source': 'AA', 'delay': 30, 'target': 'BB'}})
         dynamic_events = DynamicEvents()
         delay_events.setup(dynamic_events)
         self.assertEqual(delay_events._delay_items[0].timer, 0)
@@ -81,3 +81,54 @@ class TestEventToEvent(unittest.TestCase):
         delay_events.update()
         self.assertLess(delay_events._delay_items[0].timer, 29.75)
         self.assertGreater(delay_events._delay_items[0].timer, 29.5)
+
+    def test_expired_delay(self):
+        delay_events = DelayEvents({'example': {'source': 'AA', 'delay': 10, 'target': 'BB'}})
+        dynamic_events = DynamicEvents()
+        delay_events.setup(dynamic_events)
+        # trigger source event, in order to trigger the delayed call to the target event
+        dynamic_events.getEvent('AA').fire()
+        delay_events.update(10) # let 10 seconds 'pass'
+        self.assertEqual(dynamic_events.getEvent('AA')._fireCount, 1)
+        self.assertEqual(dynamic_events.getEvent('BB')._fireCount, 1) # B got fired
+        delay_events.update(10) # let's move another 10 seconds into the future
+        self.assertEqual(dynamic_events.getEvent('AA')._fireCount, 1)
+        self.assertEqual(dynamic_events.getEvent('BB')._fireCount, 1) # B didn't get fired again
+
+    def test_delay_runs_multiple_times(self):
+        delay_events = DelayEvents({'example': {'source': 'AA', 'delay': 10, 'target': 'BB'}})
+        dynamic_events = DynamicEvents()
+        delay_events.setup(dynamic_events)
+        # trigger source event, in order to trigger the delayed call to the target event
+        dynamic_events.getEvent('AA').fire()
+        delay_events.update(10) # let 10 seconds 'pass'
+        self.assertEqual(dynamic_events.getEvent('AA')._fireCount, 1)
+        self.assertEqual(dynamic_events.getEvent('BB')._fireCount, 1) # B got fired
+        delay_events.update(10) # let's move another 10 seconds into the future
+        self.assertEqual(dynamic_events.getEvent('AA')._fireCount, 1)
+        self.assertEqual(dynamic_events.getEvent('BB')._fireCount, 1) # B didn't get fired again
+        dynamic_events.getEvent('AA').fire()
+        self.assertEqual(dynamic_events.getEvent('AA')._fireCount, 2)
+        self.assertEqual(dynamic_events.getEvent('BB')._fireCount, 1) # B didn't get fired again
+        delay_events.update(10) # let 10 seconds 'pass'
+        self.assertEqual(dynamic_events.getEvent('AA')._fireCount, 2)
+        self.assertEqual(dynamic_events.getEvent('BB')._fireCount, 2) # B didn't get fired again
+
+    def test_looping_event(self):
+        delay_events = DelayEvents({'looper': {'source': 'loop', 'delay': 10, 'target': 'loop'}})
+        dynamic_events = DynamicEvents()
+        delay_events.setup(dynamic_events)
+        delay_events.update(10) # let 10 seconds 'pass'
+        self.assertEqual(dynamic_events.getEvent('loop')._fireCount, 0)
+        # trigger source event, in order to trigger the delayed call to the target event
+        dynamic_events.getEvent('loop').fire()
+        self.assertEqual(dynamic_events.getEvent('loop')._fireCount, 1)
+        delay_events.update(5) # halfway towards the second trigger
+        self.assertEqual(dynamic_events.getEvent('loop')._fireCount, 1)
+        delay_events.update(5) # time for the second trigger
+        self.assertEqual(dynamic_events.getEvent('loop')._fireCount, 2)
+        delay_events.update(10) # 3rd
+        self.assertEqual(dynamic_events.getEvent('loop')._fireCount, 3)
+        delay_events.update(10) # 4th
+        self.assertEqual(dynamic_events.getEvent('loop')._fireCount, 4)
+        # etc

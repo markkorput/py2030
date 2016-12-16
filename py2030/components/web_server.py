a076e73c71af58dbc1d6d1c032203936779f92cc
Mark van de Korput <dr.theman@gmail.com>
2016-12-16 17:09:38 +0100

    WebServer components output_events feature works

--- py2030/components/web_server.py
+++ py2030/components/web_server.py
@@ -1,17 +1,56 @@
-import logging, threading, time, socket, httplib
+import logging, threading, time, socket, httplib, os
 
 from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-class LocalRequestHandler(SimpleHTTPRequestHandler):
-    def translate_path(self, path):
-        # print '[MyRequestHandler] translate_path:', path
-        if path.endswith('config.yaml'):
-            #     print 'assumed config.yaml'
-            return SimpleHTTPRequestHandler.translate_path(self, '/config/config.yaml')
+def createRequestHandler(event_manager = None, _options = {}):
+    class CustomHandler(SimpleHTTPRequestHandler, object):
+        def __init__(self, *args, **kwargs):
+            # do_stuff_with(self, init_args)
+            self.options = _options
+            self.root_path = self.options['serve'] if 'serve' in _options else '.'
+            self.event_manager = event_manager
+            super(CustomHandler, self).__init__(*args, **kwargs)
 
-        return SimpleHTTPRequestHandler.translate_path(self, path)
+        def process_request(self):
+            if self.event_manager != None and 'output_events' in self.options:
+                if self.path in self.options['output_events']:
+                    self.event_manager.fire(self.options['output_events'][self.path])
+                    # self.send_error(204)
+                    self.send_response(200)
+                    self.end_headers()
+                    return True
+            return False
 
+        def do_HEAD(self):
+            if self.process_request():
+                return
+            super(CustomHandler, self).do_HEAD()
+
+        def do_GET(self):
+            if self.process_request():
+                return
+            super(CustomHandler, self).do_GET()
+
+        def do_POST(self):
+            if self.process_request():
+                return
+            super(CustomHandler, self).do_POST()
+
+        def translate_path(self, path):
+            if self.event_manager != None and 'output_events' in self.options:
+                if path in self.options['output_events']:
+                    self.event_manager.fire(self.options['output_events'][path])
+                    # self.send_error(204)
+                    self.send_response(200)
+                    self.wfile.write('OK')
+                    self.wfile.close()
+                    return ''
+
+            relative_path = path[1:] if path.startswith('/') else path
+            return SimpleHTTPRequestHandler.translate_path(self, os.path.join(self.root_path, relative_path))
+
+    return CustomHandler
 
 class WebServer(threading.Thread):
     def __init__(self, options = {}):
@@ -57,7 +96,8 @@ class WebServer(threading.Thread):
     # thread function
     def run(self):
         self.logger.debug('Starting HTTP server on port {0}'.format(self.port()))
-        self.http_server = HTTPServer(('', self.port()), LocalRequestHandler)
+        HandlerClass = createRequestHandler(self.event_manager, self.options)
+        self.http_server = HTTPServer(('', self.port()), HandlerClass)
 
         # self.httpd.serve_forever()
         # self.httpd.server_activate()
@@ -78,12 +118,12 @@ class WebServer(threading.Thread):
 # for testing
 if __name__ == '__main__':
     logging.basicConfig()
-    ws = WebServer({'verbose': True})
+    ws = WebServer({'verbose': True, 'serve': 'examples'})
     try:
         ws.setup()
         while True:
             time.sleep(.1)
     except KeyboardInterrupt:
         print 'KeyboardInterrupt. Quitting.'
-        ws.destroy()
-        print 'thread closed'
+
+    ws.destroy()
--- tests/test_web_server.py
+++ tests/test_web_server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import unittest
+import unittest, httplib, os
 from py2030.components.web_server import WebServer
 from py2030.event_manager import EventManager
 
@@ -15,6 +15,7 @@ class TestWebServer(unittest.TestCase):
         webserver.setup()
         self.assertTrue(webserver.isAlive())
         self.assertIsNone(webserver.event_manager)
+        webserver.destroy()
 
     def test_setup_with_event_manager(self):
         webserver = WebServer()
@@ -22,6 +23,7 @@ class TestWebServer(unittest.TestCase):
         webserver.setup(em)
         self.assertTrue(webserver.isAlive())
         self.assertEqual(webserver.event_manager, em)
+        webserver.destroy()
 
     def test_destroy(self):
         webserver = WebServer()
@@ -32,3 +34,39 @@ class TestWebServer(unittest.TestCase):
         webserver.destroy()
         self.assertFalse(webserver.isAlive())
         self.assertIsNone(webserver.event_manager)
+
+    def test_port_default_value(self):
+        self.assertEqual(WebServer().port(), 2031)
+
+    def test_port_config_option(self):
+        self.assertEqual(WebServer({'port': 1234}).port(), 1234)
+
+    def test_request(self):
+        webserver = WebServer()
+        webserver.setup()
+        connection = httplib.HTTPConnection('127.0.0.1', webserver.port())
+        connection.request('GET', '/tests/data/config.yml')
+        response = connection.getresponse().read()
+        with open(os.path.join(os.path.dirname(__file__), 'data', 'config.yml')) as f:
+            self.assertEqual(response, f.read())
+        webserver.destroy()
+
+    def test_serve_options(self):
+        webserver = WebServer({'serve': 'tests/data'})
+        webserver.setup()
+        connection = httplib.HTTPConnection('127.0.0.1', webserver.port())
+        connection.request('GET', 'config.yml')
+        response = connection.getresponse().read()
+        with open(os.path.join(os.path.dirname(__file__), 'data', 'config.yml')) as f:
+            self.assertEqual(response, f.read())
+        webserver.destroy()
+
+    def test_output_event(self):
+        webserver = WebServer({'output_events': {'/api/start': 'startEvent'}})
+        em = EventManager()
+        webserver.setup(em)
+        connection = httplib.HTTPConnection('127.0.0.1', webserver.port())
+        self.assertEqual(em.get('event1')._fireCount, 0)
+        connection.request('GET', '/api/start')
+        webserver.destroy()
+        self.assertEqual(em.get('startEvent')._fireCount, 1)

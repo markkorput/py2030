a00f8e3e20bdd6ebd86ab130bc63901d1e73b1ba
mark <mark.korput@gmail.com>
2017-09-24 13:41:41 +0200

    actual DMX output using pysimpledmx

--- py2030/components/dmx_output.py
+++ py2030/components/dmx_output.py
@@ -4,22 +4,24 @@ import logging
 from evento import Event
 from py2030.base_component import BaseComponent
 
+try:
+    import pysimpledmx
+except ImportError:
+    pysimpledmx = None
+
 class DmxOutput(BaseComponent):
     config_name = 'dmx_outputs'
 
     def __init__(self, options = {}):
-        # params
         self.options = options
-        # attributes
-        # self.port = self.options['port'] if 'port' in self.options else None
-        # self.midiin = None
-        # self.port_name = None
-        # self.limit = 10
-        # self.connected = False
-        self.num_channels = self.options['num_channels'] if 'num_channels' in self.options else 10
-        self.channel_event_prefix = self.options['channel_event_prefix'] if 'channel_event_prefix' in self.options else 'ch'
         self.event_manager = None
         self.output_events = None
+
+        self.num_channels = self.options['num_channels'] if 'num_channels' in self.options else 10
+        self.channel_event_prefix = self.options['channel_event_prefix'] if 'channel_event_prefix' in self.options else 'ch'
+        self.deviceName = self.options['deviceName'] if 'deviceName' in self.options else None
+        self.deviceNumber = int(self.options['deviceNumber']) if 'deviceNumber' in self.options else None
+
         self.fps = self.options['fps'] if 'fps' in self.options else 4.0
         self.frameTime = 1.0 / self.fps
 
@@ -35,6 +37,18 @@ class DmxOutput(BaseComponent):
     def setup(self, event_manager=None):
         self.event_manager = event_manager
 
+        if not pysimpledmx:
+            self.logger.warn("pysimpledmx lib not loaded")
+            self.dmx = None
+        else:
+            if self.deviceName != None:
+                self.dmx = pysimpledmx.DMXConnection(self.deviceName)
+            elif self.deviceNumber != None:
+                self.dmx = pysimpledmx.DMXConnection(self.deviceNumber)
+
+        if self.dmx:
+            self.dmx.clear()
+
         for i in range(self.num_channels):
             evt = self.event_manager.get(self.channel_event_prefix+str(i))
             evt += lambda val, idx=i: self._setChannel(idx, val)
@@ -45,11 +59,20 @@ class DmxOutput(BaseComponent):
         t = time.time()
         if self.dirty and t >= self.nextFrameTime:
             # dmx update
+            if self.dmx != None:
+                self.dmx.render()
+
             self.logger.debug('DMX update')
             self.nextFrameTime = t + self.frameTime
             self.dirty = False
 
     def destroy(self):
+        if self.dmx:
+            self.dmx.clear()
+            self.dmx.render()
+            self.dmx.close()
+            self.dmx = None
+
         # remove events from event manager, good idea?
         for i in range(self.num_channels):
             self.event_manager.remove(self.channel_event_prefix+str(i))
@@ -59,4 +82,6 @@ class DmxOutput(BaseComponent):
 
     def _setChannel(self, idx, val):
         # self.logger.debug('_setChannel: '+str(idx)+' with: '+str(val))
+        if self.dmx != None:
+            self.dmx.setChannel(idx+1, int(val * 255.0))
         self.dirty = True
